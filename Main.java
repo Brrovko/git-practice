5a52a7a0b740d8c7c7f4c8079dd4fbd854b5a0da
Andrey Brovko <aybrovko@avito.ru>
2024-05-14 19:58:25 +0300

    Feature-branch

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testers is awesome!");
+        System.out.println("Feature branch");
     }
   
 }
