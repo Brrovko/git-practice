053ea4ae94e13df78b24ccbd6c1a5f25d0eeb102
Andrei Brovko <brov.ko@ya.ru>
2024-05-14 19:47:40 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("SDET is awesome!");
+        System.out.println("QA is awesome!");
     }
   
 }
